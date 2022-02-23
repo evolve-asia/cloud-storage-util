77e81b4fdfe3059a352f86d98225309cbb22e0cb
Nabin Khatiwada <nawin@Nawins.local>
2022-02-23 14:25:49 +0545

    made nullable

--- cloudstorageutil/src/main/java/com/evolveasia/aws/S3ObjectSummary.kt
+++ cloudstorageutil/src/main/java/com/evolveasia/aws/S3ObjectSummary.kt
@@ -3,12 +3,12 @@ package com.evolveasia.aws
 import java.util.*
 
 class S3ObjectSummary(
-    var key: String,
-    var eTag: String,
-    var lastModified: Date,
-    var storageClass: String,
+    var key: String?,
+    var eTag: String?,
+    var lastModified: Date?,
+    var storageClass: String?,
     var owner: String?,
     var ownerId: String?,
-    var size: Long,
-    var baseUrl: String
+    var size: Long?,
+    var baseUrl: String?
 )
